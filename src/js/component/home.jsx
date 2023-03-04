971f495cae7e4e470e251e7c889a02e9ca35ff45
Anthonycalvo1984 <anthonycal1984@gmail.com>
2023-03-04 02:14:32 +0000

    entrega

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import "../../styles/traffic.css"
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	const[active, setActive] = useState("")
+	useEffect(()=>{console.log(active)},[active])
+
+  return (<>
+      <h1 className="text-center mt-5">Traffic Light</h1>
+     
+          <div className={"rounded-circle " + `red-light` + `${active==="rounded-circle red-light"? " red-light-active" : ""}`} onClick={(e)=>{setActive(e.target.className)}}>Red</div> 
+		  <br/>
+          <div className={"rounded-circle " + `yellow-light` + `${active==="rounded-circle yellow-light"? " yellow-light-active" : ""}`} onClick={(e)=>{setActive(e.target.className)}}>Yellow</div> 
+		  <br/>
+          <div className={"rounded-circle " + `green-light` + `${active==="rounded-circle green-light"? " green-light-active" : ""}`} onClick={(e)=>{setActive(e.target.className)}}>Green</div> 
+        
+      </>
+    
+  );
 };
 
 export default Home;
